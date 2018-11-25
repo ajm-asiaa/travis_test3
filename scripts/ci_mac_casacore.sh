67e018e6dc72a2ab3ee4db68875ecd283096cbda
ajm-asiaa <ajm@asiaa.sinica.edu.tw>
2018-11-25 10:21:47 +0800

    test 5

--- scripts/ci_mac_casacore.sh
+++ scripts/ci_mac_casacore.sh
@@ -13,10 +13,6 @@ mv casacore casacore-src
 
 cd casacore-src && mkdir build && cd build
 
-brew install boost-python
-
-ls /usr/local/Cellar/boost-python/
-
 #Fix so that cmake can find the boost-python library file
 ln -s /usr/local/Cellar/boost-python/1.68.0/lib/libboost_python27-mt.dylib \
       /usr/local/lib/libboost_python-mt.dylib
